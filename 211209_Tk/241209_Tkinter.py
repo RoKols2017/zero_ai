77afd1a665ab4e3ee1074ab5ba7a1dee6d88ae29
Роман <RoKols2017@gmail.com>
2024-12-09 18:21:12 +0300

    Размещение элементов с помощью Grid исправлено

--- 211209_Tk/241209_Tkinter.py
+++ 211209_Tk/241209_Tkinter.py
@@ -6,7 +6,7 @@ def input_name():
         name = "Медвед"
 
     label = tk.Label(root)
-    label.grid(row=1, column=0)
+    label.grid(row=1, column=1)
     label.config(text="")
     label.config(text=f"Привет {name}!")
 
@@ -19,12 +19,12 @@ label = tk.Label(root, text="Введите свое имя:")
 label.grid(row=0, column=0)
 
 entry = tk.Entry(root)
-entry.grid(row=1, column=1)
+entry.grid(row=0, column=1)
 
 button = tk.Button(root, text="Скорее нажми на кнопку", command = input_name)
-button.grid(row=2, column=1)
+button.grid(row=1, column=0)
 
 button = tk.Button(root, text="Нажми для завершения", command=root.destroy)
-button.grid(row=5, column=3)
+button.grid(row=2, column=0)
 
 root.mainloop()
